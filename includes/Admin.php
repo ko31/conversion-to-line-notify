eab173b73b20375163d1b51fbc3132251db0f2eb
Ko Takagi <knockoutmarch@gmail.com>
2020-09-30 09:20:09 +0900

    :+1: Add token check

--- includes/Admin.php
+++ includes/Admin.php
@@ -79,14 +79,6 @@ class Admin {
 		);
 
 		add_settings_field(
-			'line_access_token_checker',
-			'',
-			[ $this, 'line_access_token_checker_callback' ],
-			$this->option_group,
-			'general_settings'
-		);
-
-		add_settings_field(
 			'is_order_enabled',
 			__( 'Order', 'conversion-to-line-notify' ),
 			[ $this, 'is_order_enabled_callback' ],
@@ -113,21 +105,19 @@ class Admin {
 		       value="<?php echo $line_access_token; ?>"
 		       class="regular-text">
 		<?php
-	}
-
-	/**
-	 * Render line_access_token_checker field.
-	 */
-	public function line_access_token_checker_callback() {
-		$line_access_token = isset( $this->options['line_access_token'] ) ? $this->options['line_access_token'] : '';
 		if ( $line_access_token ) :
 			$line = new Line();
-			if ( $line->status() ) {
-				echo '<p class="description">' . __( 'LINE access token is valid.', '' ) . '</p>';
-			} else {
-				echo '<p class="description"><strong>' . __( 'LINE access token is invalid! Please check again.', '' ) . '</strong></p>';
-			}
+			if ( $line->status() ) :
+				?>
+				<span class="dashicons dashicons-yes"></span>
+			<?php
+			else:
+				_e( 'The access token is invalid! Please check again.', 'conversion-to-line-notify' );
+			endif;
 		endif;
+		?>
+		<p>Get an access token on <a href="https://notify-bot.line.me/login" target="_blank">LINE Notify</a>.</p>
+		<?php
 	}
 
 	/**
